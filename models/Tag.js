b86373374d0a2dd080019c3ef04d026b1245a7f5
eeast <eeasthope@gmail.com>
2023-05-06 11:53:02 -0500

    working mvp

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
   },
   {
     sequelize,
